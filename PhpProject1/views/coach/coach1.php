5bd1124b8ef9bfe654a70788d7c60541455fe3b5
FisherMD <FisherMD09@uww.edu>
2017-04-03 17:48:41 -0500

    Fixed verbiage, Rearranged signup pages

--- PhpProject1/views/coach/coach1.php
+++ PhpProject1/views/coach/coach1.php
@@ -49,7 +49,7 @@ include('Assets/pageHeader.php');
 				<div class="cols-sm-10">
 					<div class="input-group">
 						<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-						<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your email address"/>
+						<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
 					</div>
 				</div>	
 			</div>
@@ -73,6 +73,68 @@ include('Assets/pageHeader.php');
 				</div>	
 			</div>
                         <hr>
+                        <!--Bio Input-->
+                        <div class="form-group">
+				<label class="cols-sm-2 control-label" for="state">Which state are you based out of?</label>
+				<div class="cols-sm-10">
+					<div class="input-group">		
+						<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+						<select id="state" name="state" class="form-control">
+							<option value="0">Select a State</option>
+							<option value="1">Alabama</option>
+							<option value="2">Alaska</option>
+							<option value="3">Arizona</option>
+							<option value="4">Arkansas</option>
+							<option value="5">California</option>
+							<option value="6">Colorado</option>
+							<option value="7">Connecticut</option>
+							<option value="8">Delaware</option>
+							<option value="9">Florida</option>
+							<option value="10">Georgia</option>
+							<option value="11">Hawaii</option>
+							<option value="12">Idaho</option>
+							<option value="13">Illinois</option>
+							<option value="14">Indiana</option>
+							<option value="15">Iowa</option>
+							<option value="16">Kansas</option>
+							<option value="17">Kentucky</option>
+							<option value="18">Louisiana</option>
+							<option value="19">Maine</option>
+							<option value="20">Maryland</option>
+							<option value="21">Massachusetts</option>
+							<option value="22">Michigan</option>
+							<option value="23">Minnesota</option>
+							<option value="24">Mississippi</option>
+							<option value="25">Missouri</option>
+							<option value="26">Montana</option>
+							<option value="27">Nebraska</option>
+							<option value="28">Nevada</option>
+							<option value="29">New Hampshire</option>
+							<option value="30">New Jersey</option>
+							<option value="31">New Mexico</option>
+							<option value="32">New York</option>
+							<option value="33">North Carolina</option>
+							<option value="34">North Dakota</option>
+							<option value="35">Ohio</option>
+							<option value="36">Oklahoma</option>
+							<option value="37">Oregon</option>
+							<option value="38">Pennsylvania</option>
+							<option value="39">Rhode Island</option>
+							<option value="40">South Carolina</option>
+							<option value="41">South Dakota</option>
+							<option value="42">Tennessee</option>
+							<option value="43">Texas</option>
+							<option value="44">Utah</option>
+							<option value="45">Vermont</option>
+							<option value="46">Virginia</option>
+							<option value="47">Washington</option>
+							<option value="48">West Virginia</option>
+							<option value="49">Wisconsin</option>
+							<option value="50">Wyoming</option>
+						</select>
+					</div>			
+				</div>					
+			</div>
                         <!-- School input -->
 			<div class="form-group">
 				<label for="school" class="cols-sm-2 control-label">Name of School or Club</label>
@@ -150,68 +212,7 @@ include('Assets/pageHeader.php');
 					</div>
 				</div>
 			</div>
-			<div class="form-group">
-				<label class="cols-sm-2 control-label" for="state">Which state are you based out of?</label>
-				<div class="cols-sm-10">
-					<div class="input-group">		
-						<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-						<select id="state" name="state" class="form-control">
-							<option value="0">Select a State</option>
-							<option value="1">Alabama</option>
-							<option value="2">Alaska</option>
-							<option value="3">Arizona</option>
-							<option value="4">Arkansas</option>
-							<option value="5">California</option>
-							<option value="6">Colorado</option>
-							<option value="7">Connecticut</option>
-							<option value="8">Delaware</option>
-							<option value="9">Florida</option>
-							<option value="10">Georgia</option>
-							<option value="11">Hawaii</option>
-							<option value="12">Idaho</option>
-							<option value="13">Illinois</option>
-							<option value="14">Indiana</option>
-							<option value="15">Iowa</option>
-							<option value="16">Kansas</option>
-							<option value="17">Kentucky</option>
-							<option value="18">Louisiana</option>
-							<option value="19">Maine</option>
-							<option value="20">Maryland</option>
-							<option value="21">Massachusetts</option>
-							<option value="22">Michigan</option>
-							<option value="23">Minnesota</option>
-							<option value="24">Mississippi</option>
-							<option value="25">Missouri</option>
-							<option value="26">Montana</option>
-							<option value="27">Nebraska</option>
-							<option value="28">Nevada</option>
-							<option value="29">New Hampshire</option>
-							<option value="30">New Jersey</option>
-							<option value="31">New Mexico</option>
-							<option value="32">New York</option>
-							<option value="33">North Carolina</option>
-							<option value="34">North Dakota</option>
-							<option value="35">Ohio</option>
-							<option value="36">Oklahoma</option>
-							<option value="37">Oregon</option>
-							<option value="38">Pennsylvania</option>
-							<option value="39">Rhode Island</option>
-							<option value="40">South Carolina</option>
-							<option value="41">South Dakota</option>
-							<option value="42">Tennessee</option>
-							<option value="43">Texas</option>
-							<option value="44">Utah</option>
-							<option value="45">Vermont</option>
-							<option value="46">Virginia</option>
-							<option value="47">Washington</option>
-							<option value="48">West Virginia</option>
-							<option value="49">Wisconsin</option>
-							<option value="50">Wyoming</option>
-						</select>
-					</div>			
-				</div>					
-			</div>
-			
+						
 			<!-- City Input -->
 			<div class="form-group">
 				<label for="city" class="cols-sm-2 control-label">City</label>

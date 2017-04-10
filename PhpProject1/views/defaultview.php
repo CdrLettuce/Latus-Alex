78cefb8fef87f3a0babf174731a71a0c8c1aea7a
butlersj02 <butlersj02@uww.edu>
2017-04-10 17:27:47 -0500

    -css tweaks to the default page 	-Logo added 	-color tweaks to glyphcons and headers

--- PhpProject1/views/defaultview.php
+++ PhpProject1/views/defaultview.php
@@ -4,7 +4,7 @@ include('Assets/pageHeader.php');
 	<!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron minheight">
       <div class="container">
-        <h1>Step Up, Be Noticed, Get Recruited</h1>
+        <h1>Recruit Smarter, Not Harder</h1>
         <p>Your recruitment journey begins here</p>
         <p><a class="btn btn-primary btn-lg" href="#learnMore" role="button">Learn more &raquo;</a></p>
       </div>
@@ -18,14 +18,14 @@ include('Assets/pageHeader.php');
         </div>
         <div class="col-md-6 icon">
           <h2>Players</h2>
-          <span class="glyphicon glyphicon-user logo"></span>
+          <span class="glyphicon glyphicon-user logo blue"></span>
           <p>Find the perfect college team for YOU. Create a profile, upload video highlights, pictures and stats, then connect with college coaches across the nation to show why their team needs YOU!</p>
           <p><a class="btn btn-primary" id="" href="<?php echo HOME;?>signup/playerSignup1" role="button">Player Sign Up &raquo;</a></p>
         </div>
         <div class="col-md-6 icon">
           <h2>Coaches</h2>
-          <span class="glyphicon glyphicon-list-alt logo"></span>
-          <p>Find the perfect players for your college team. Search the nation for specific players base on age, location, position, academic performance, and much more! </p>
+          <span class="glyphicon glyphicon-list-alt logo blue"></span>
+          <p>Find the perfect players for your college team. Search the nation for specific players based on age, location, position, academic performance, and much more! </p>
           <p><a class="btn btn-primary" id="" href="<?php echo HOME;?>signup/coachSignup1" role="button">Coach Sign Up &raquo;</a></p>
        </div>
       </div>
@@ -82,17 +82,17 @@ include('Assets/pageHeader.php');
         </div>
         
 	 <div class="col-md-4 icon">
-          <i class="fa fa-mobile-phone fa-5x" aria-hidden="true"></i>
+          <i class="fa fa-mobile-phone fa-5x blue" aria-hidden="true"></i>
           <p>Access your profile, message other players and coaches, and upload new content all while on the go! RecruitChute is mobile-friendly and available anywhere you go.</p>
           <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
         </div>
         <div class="col-md-4 icon">
-          <i class="fa fa-graduation-cap fa-5x" aria-hidden="true"></i>
+          <i class="fa fa-graduation-cap fa-5x blue" aria-hidden="true"></i>
           <p>We give high school athletes the opportunity to gain an education while competing in the sport they love. </p>
           <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
         </div>
         <div class="col-md-4 icon">
-          <i class="fa fa-globe fa-5x" aria-hidden="true"></i>
+          <i class="fa fa-globe fa-5x blue" aria-hidden="true"></i>
           <p>RecruitChute keeps players and coaches connected to their communities. One percent of all revenue is donated to local youth sports programs. </p>
           <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
        </div>

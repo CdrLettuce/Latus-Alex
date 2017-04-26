7d32ac4b4b9d7657dd80a1b25810a41407d10144
Schifty87 <ryan.j.schiferl@gmail.com>
2017-04-26 08:24:39 -0500

    revised "about us" page

--- PhpProject1/views/aboutUs.php
+++ PhpProject1/views/aboutUs.php
@@ -1,51 +1,69 @@
 <?php
 include('Assets/pageHeader.php');
 ?>
-<div class="container h gray about">
-    <div class="b">
-        <h2 class="section">Mission Statement</h2>
-        <p class="pbody">RecruitChute's mission is to provide the tools and guidance necessary to assist prospective college soccer players 
-        throughout the recruitment journey from start to finish. If you have the drive, we have the tools and don't let anyone 
-        tell you you're not good enough. Soccer is the number one growing sport in the US and for good reason - Soccer is awesome! 
-        Here at RecruitChute, we live and breathe soccer in our daily lives, all members of our team have played at least high school 
-        soccer so we like to think we know a thing or two about how this process works! </p>
-    </div>
-    <div class="b">
-        <h2 class="section">Our Philosophy</h2>
-        <p class="pbody">Here at RecruitChute we have always had the firm belief that when it comes to athletic recruiting, the more proactive you 
-        can be, the better. College coaches want to deal with athletes directly, not their parents or middle-men. The best way to 
-        market yourself effectively to college coaches is to do exactly that, market YOURSELF. We provide the tools that allow YOU to 
-        make it happen. 
-        </p>
-    </div>
-    <div class="b">
-        <h2 class="section">Founder</h2>
+<div class="gray about">
+    <div class="container">
         <div class="row">
-            <div class="col-xs-6">
-                <div class="card">
-                    <div class="image-cropper">
-                        <img class="resize" src="../images/player.jpg" alt="Avatar" style="width:100%">
-                    </div>
-                    <div class="card-c">
-                        <h4><b>Christopher Roche</b></h4> 
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
+            <div class="col-sm-offset-1 col-sm-5 mission">
+                <h2 class="section">RecruitChute's Mission</h2>
+                <p class="pbody">RecruitChute's mission is to provide the tools and guidance necessary to assist prospective college soccer players 
+                throughout the recruitment journey from start to finish. If you have the drive, we have the tools and don't let anyone 
+                tell you you're not good enough. Soccer is the number one growing sport in the US and for good reason - Soccer is awesome! 
+                Here at RecruitChute, we live and breathe soccer in our daily lives, all members of our team have played at least high school 
+                soccer so we like to think we know a thing or two about how this process works! 
+                </p>      
             </div>
-            <div class="col-xs-6">
-                <blockquote cite="http://www.worldwildlife.org/who/index.html">“I have always, and will always, believe that every athlete
-                should be given the opportunity to play college soccer as it is such a once in a lifetime experience.  I 
-                founded RecruitChute to give every soccer player in this country the best chance of living their dream out of playing 
-                college soccer – and if you get the shot, take it. To this day, I still have a strong relationship with all my old 
-                teammates, current teammates and coaching staff because of the unbreakable bond that was formed throughout my four-year 
-                collegiate soccer career.”  
-                </blockquote>
+            <div class="col-sm-offset-0 col-sm-5 mission">
+                <h2 class="section">RecruitChute's Philosophy</h2>
+                <p class="pbody">Here at RecruitChute we have always had the firm belief that when it comes to athletic recruiting, the more proactive you 
+                can be, the better. College coaches want to deal with athletes directly, not their parents or middle-men. The best way to 
+                market yourself effectively to college coaches is to do exactly that, market YOURSELF. We provide the tools that allow YOU to 
+                make it happen. 
+                </p>
             </div>
         </div>
-        <p class="pbody">RecruitChute was founded by Manchester, England native Christopher Roche who was recruited over to play college soccer 
-        over in Wisconsin. As a life-time Manchester United fan, he considers himself to know a thing or two about the sport and has 
-        been implementing his soccer theories and style of play throughout his coaching career. Now in the college recruiting industry, 
-        Chris is revolutionizing the way that high school athletes and college coaches interact and communicate. </p>
+    </div>
+    
+    
+    
+    <div class="b">
+        <div class ="container">
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-5">
+                    <h2 class="section">Founder</h2>
+                    <div class="card">
+                        <div class="image-cropper">
+                            <img class="resize" src="../images/player.jpg" alt="Avatar" style="width:100%">
+                        </div>
+                        <div class="card-c">
+                            <h4><b>Christopher Roche</b></h4> 
+                            <p>Architect & Engineer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-offset-0 col-sm-5">
+                    <blockquote cite="http://www.worldwildlife.org/who/index.html">“I have always, and will always, believe that every athlete
+                    should be given the opportunity to play college soccer as it is such a once in a lifetime experience.  I 
+                    founded RecruitChute to give every soccer player in this country the best chance of living their dream out of playing 
+                    college soccer – and if you get the shot, take it. To this day, I still have a strong relationship with all my old 
+                    teammates, current teammates and coaching staff because of the unbreakable bond that was formed throughout my four-year 
+                    collegiate soccer career.”  
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="container started">
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-10">
+                    <h2 class = "section">How RecruitChute Started</h2>
+                    <h4 class="pbody">RecruitChute was founded by Manchester, England native Christopher Roche who was recruited over to play college soccer 
+                    over in Wisconsin. As a life-time Manchester United fan, he considers himself to know a thing or two about the sport and has 
+                    been implementing his soccer theories and style of play throughout his coaching career. Now in the college recruiting industry, 
+                    Chris is revolutionizing the way that high school athletes and college coaches interact and communicate. </h4>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </div>
 <?php include('Assets/pageFooter.php');

5a2b6fbf6901ff07de8fe09e2411daa5a728e434
butlersj02 <butlersj02@uww.edu>
2017-05-03 16:56:17 -0500

    functions and pages for favorites are complete, will continue to implement around the other sites

--- PhpProject1/models/favoritemodel.php
+++ PhpProject1/models/favoritemodel.php
@@ -30,6 +30,20 @@ class favoriteModel extends Model
                     $result = -1;
             return $result;
         }
+        public function getfavoritedplayers2(){           
+            $sql = "SELECT followed_id
+                    FROM favorites
+                    WHERE follower_id = :userId";
+            $this->setSql($sql);
+            $data = json_decode(file_get_contents('php://input'));
+            $values = array(':userId'=>$data->user_id);
+            $objects = $this->getAll($values);
+            $ids = $objects->followed_id;
+            /* if the user is not found then return -1 */
+            if (!$ids)
+                    $ids = -1;
+            return $ids;
+        }
         
         public function getfavoritedplayersinfo(){
 
@@ -38,22 +52,26 @@ class favoriteModel extends Model
                 */
                 $sql = "SELECT u.first_name, u.last_name, u.city, s.state_name, u.user_id, p.graduation_date, o.position
                         FROM `users` as u, `states` as s, `player_profile` as p, `position` as o
-                        WHERE u.user_id = :user_id)";
+                        WHERE u.user_id = :user_id
+                        AND u.state_id = s.state_id
+                        AND p.position_1 = o.position_id
+                        AND u.user_id = p.user_id";
                 $this->setSql($sql);
                 // prepare SQL statement
                 $stm = $this->_db->prepare($sql);
                 // read input
                 $data = json_decode(file_get_contents('php://input'));
-                $customer_id = $data->customer_id;
-                $products = $data->products;
-                // execute SQL statement using each product in $products array
-                for ($i=0; $i<count($products); $i++){
-                        $parameters = array(':product_id'=>$products[$i]->product_id,
-                                          ':customer_id'=>$customer_id,
-                                          ':quantity'=>$products[$i]->quantity);
+                $followed = $data->ids;
+                // execute SQL statement using each followed person in $user_id array
+                for ($i=0; $i<count($followed); $i++){
+                        $parameters = array(':user_id'=>$followed[$i]->followed_id);
                         $stm->execute($parameters);
+                        $result[$i] = $stm->fetch();
                 }
-                return 1;
+                /* if the user is not found then return -1 */
+                if (!$result)
+                        $result = -1;
+                return $result;
         }
         
         public function addItemsToSales(){
@@ -76,6 +94,7 @@ class favoriteModel extends Model
                                           ':customer_id'=>$customer_id,
                                           ':quantity'=>$products[$i]->quantity);
                         $stm->execute($parameters);
+                        
                 }
                 return 1;
         }

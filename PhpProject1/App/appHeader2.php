e13119a181c747bc2318f05792700653f0af6425
Schifty87 <ryan.j.schiferl@gmail.com>
2017-05-04 12:14:18 -0500

    new things!

--- PhpProject1/App/appHeader2.php
+++ PhpProject1/App/appHeader2.php
@@ -9,14 +9,16 @@
               </button>
               <a class="navbar-brand" href="http://recruitchute.io/index.php" style='padding-top:10px;'><img src="../Assets/images/logo_trans.png" alt="logo" height="55" width="160"></a>
             </div>
-            <div id="navbar" class="navbar-collapse collapse navbar-right link">                 
-                    <a class="navbar-brand home"  href="#/" style=""><i class="fa fa-home fa-1x blue home" aria-hidden="true" style=""></i><p>Home </p></a>                
+            <div id="navbar" class="navbar-collapse collapse navbar-right link container">
+                <div class="row">
+                    <a class="navbar-brand"  href="#/" style=""><i class="fa fa-home fa-1x blue" aria-hidden="true" style=""></i><p>Home </p></a>                
                     <a class="navbar-brand"  href="#/search"><i class="fa fa-binoculars fa-1x blue" aria-hidden="true" style=""></i><p>Search</p></a>                        
                     <a class="navbar-brand"  href="#/network"><i class="fa fa-users fa-1x blue" aria-hidden="true" style=""></i><p>My Network</p></a>                                 
                     <a class="navbar-brand"  href="#/messages"><i class="fa fa-comment-o fa-1x blue" aria-hidden="true" style=""></i><p>Messages</p></a>                   
                     <a class="navbar-brand"  href="#/profile"><i class="fa fa-user-circle fa-1x blue" aria-hidden="true" style=""></i><p>My Profile</p></a>  
                     <a class="navbar-brand"  href="#/login"><i class="fa fa-power-off fa-1x blue" aria-hidden="true" style=""></i><p>Sign Out</p></a>
                 </div>
+            
             </div><!--/.navbar-collapse -->
           </div>
         </nav>

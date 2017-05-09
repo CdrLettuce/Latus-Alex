b91a0f0afafd396bc75d85fe32de31d69e5ab428
Schifty87 <ryan.j.schiferl@gmail.com>
2017-05-08 20:08:38 -0500

    Fixed appHeader1 so it would be identical to others

--- PhpProject1/App/appHeader1.php
+++ PhpProject1/App/appHeader1.php
@@ -1,21 +1,23 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="http://recruitchute.io/index.php" style='padding-top:10px;'><img src="../Assets/images/logo_trans.png" alt="logo" height="55" width="160"></a>
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="http://recruitchute.io/index.php" style='padding-top:10px;'><img src="../Assets/images/logo_trans.png" style="width:60%"></a>
+                </div>
+                <div class="container">
+                    <div id="navbar" class="navbar-collapse collapse link2">
+                        <form class="navbar-form navbar-right" action="<?php echo HOME;?>login/checkLogin" method="post">
+                            <!--<button type="submit" id='sign-in' class="btn btn-success">Sign in</button>-->
+                            <a class="btn btn-success" id="" href="<?php echo HOME;?>App/router.html#/login" role="button">Sign In</a>
+                            <!--<button type="submit" class="btn btn-danger">Sign up</button>-->
+                            <a class="btn btn-danger" id="" href="<?php echo HOME;?>pp/bothSignup" role="button">Sign Up</a>
+                        </form>
+                    </div><!--/.navbar-collapse -->
+                </div>
             </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <form class="navbar-form navbar-right" action="http://recruitchute.io/login/checkLogin" method="post">
-                <!--<button type="submit" id='sign-in' class="btn btn-success">Sign in</button>-->
-                <a class="btn btn-success" id="" href="http://recruitchute.io/App/router.html#/login" role="button">Sign In</a>
-                <!--<button type="submit" class="btn btn-danger">Sign up</button>-->
-                <a class="btn btn-danger" id="" href="../pp/bothSignup" role="button">Sign Up</a>
-              </form>
-            </div><!--/.navbar-collapse -->
-          </div>
         </nav>
